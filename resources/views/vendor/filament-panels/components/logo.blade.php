d86561742e8270fd9cfd97173228e9501764bf98
Pacorrod <frodriguezcas@uoc.edu>
2024-05-06 17:19:17 +0200

    cambio de logo

--- resources/views/vendor/filament-panels/components/logo.blade.php
+++ resources/views/vendor/filament-panels/components/logo.blade.php
@@ -1,1 +1,3 @@
-<h1 style="color: rgb(47, 124, 224); font-weight:bold; font-size: 18px">Centro de formación</h1>
+{{-- <h1 style="color: rgb(47, 124, 224); font-weight:bold; font-size: 18px">Centro de formación</h1> --}}
+
+<img src="images/logo.png" alt="" width="80%">
