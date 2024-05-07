706c8636adc906cf977f6c94b274ec6c083e41ab
Pacorrod <frodriguezcas@uoc.edu>
2024-05-07 17:05:36 +0200

    Ruta relativa del logo

--- resources/views/vendor/filament-panels/components/logo.blade.php
+++ resources/views/vendor/filament-panels/components/logo.blade.php
@@ -1,3 +1,1 @@
-{{-- <h1 style="color: rgb(47, 124, 224); font-weight:bold; font-size: 18px">Centro de formación</h1> --}}
-
-<img src="images/logo.png" alt="" width="80%">
+<img src="/images/logo.png" alt="" width="80%">
